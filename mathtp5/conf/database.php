78980847452b509a78baeda92bf6dcdb57aa6ec9
Strive <39830920+Superman2113@users.noreply.github.com>
2019-02-01 09:06:39 +0800

    Update database.php

--- mathtp5/conf/database.php
+++ mathtp5/conf/database.php
@@ -57,13 +57,13 @@ return [
 //    // 数据库类型
 //    'type'            => 'mysql',
 //    // 服务器地址
-//    'hostname'        => 'ah.baobao027.com',
+//    'hostname'        => '127.0.0.1',
 //    // 数据库名
 //    'database'        => 'ahblog',
 //    // 用户名
 //    'username'        => 'root',
 //    // 密码
-//    'password'        => 'Enlai=8813',
+//    'password'        => '123456',
 //    // 端口
 //    'hostport'        => '',
 //    // 连接dsn
